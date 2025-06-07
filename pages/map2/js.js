ef8e2216578e6be7d17968ddee587b88d87569b7
oiwiio <6gish6@gmail.com>
2025-06-07 12:40:38 +0500

    фикс тем

--- pages/map2/js.js
+++ pages/map2/js.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     const themeSwitcher = document.getElementById('themeSwitcher');
-    if (themeSwitcher) {
-        themeSwitcher.addEventListener('click', () => {
-            document.body.classList.toggle('blue-theme');
-            localStorage.setItem('theme', document.body.classList.contains('blue-theme') ? 'blue' : 'red');
-        });
-        if (localStorage.getItem('theme') === 'blue') {
-            document.body.classList.add('blue-theme');
-        }
+    const body = document.body;
+
+    
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'blue') {
+        body.classList.add('blue-theme');
+    } else {
+        body.classList.remove('blue-theme');
     }
-}); 
+
+    if (themeSwitcher) {
+        themeSwitcher.addEventListener('click', function() {
+            body.classList.toggle('blue-theme');
+            
+    
+            const isBlueTheme = body.classList.contains('blue-theme');
+            localStorage.setItem('theme', isBlueTheme ? 'blue' : 'red');
+            
+            
+             
+        });
+    }
+});
